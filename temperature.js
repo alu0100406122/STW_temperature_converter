c0da7a6a4e38a94c168f0b9a8fecbdd1206ddbca
alu0100406122 <alu0100406122@ull.edu.es>
2015-10-07 18:42:22 +0000

    funcionando

--- temperature.js
+++ temperature.js
@@ -2,27 +2,27 @@
 
 function calculate(){
     var result;
-    var original = document.getElementById("");
+    var original = document.getElementById("original");
     var temp = original.value;
-    var regexp = ;
+    var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
     
-    var m = temp.match();   /*Cuando cumple la expresión regular*/
+    var m = temp.match(regexp);     /*Cuando cumple la expresión regular*/
     
     if (m){
-        var num = ;
-        var type = ;
-        num = parseFloat(num);
+        var num = m[1];
+        var type = m[2];
+        num = parseFloat(num);      /*string --> float*/
         if (type=='c' || type=='C'){
             result = (num * 9/5)+32;
-            result = ;
+            result = result.toFixed(2) + " Farenheit";  /*toFixed --> Redondeo*/
         }
         else{
             result = (num -32)*5/9;
-            result = ;
+            result = result.toFixed(2) + " Celsius";
         }
         converted.innerHTML = result;
     }
     else{
-        converted.innerHTML = "ERROR. Try something like '-4.2C' instead"
+        converted.innerHTML = "ERROR. Try something like '-4.2C' instead."
     }
 }
