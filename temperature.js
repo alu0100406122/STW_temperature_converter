074888557866b3535265a2c56f4ffab814ef24b2
alu0100406122 <alu0100406122@ull.edu.es>
2015-10-11 21:04:48 +0000

    modificación de expresión regular

--- temperature.js
+++ temperature.js
@@ -4,14 +4,40 @@ function calculate(){
     var result;
     var original = document.getElementById("original");
     var temp = original.value;
-    var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
     
+    //var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;   //primera version
+    var regexp = /([-+]?\d+(?:\.\d*)?)(?:[eE]([-+]?\d+))?\s*([fFcC])/;
     var m = temp.match(regexp);     /*Cuando cumple la expresión regular*/
     
     if (m){
         var num = m[1];
-        var type = m[2];
+        var exp = m[2];
+        var type = m[3];
         num = parseFloat(num);      /*string --> float*/
+        exp = parseInt(exp);
+
+        if(m[2]!==undefined){
+            var i,j;
+            i = 1; j = 10;
+            if(exp<0){
+                exp = -exp;
+                while(i < exp){
+                    j = j * 10;
+                    i++;}
+                if(j !== 0){
+                  num = num/j;
+                }
+            }
+            else{
+              while(i < exp){
+                j = j * 10;
+                i++;
+              }
+            if(j !== 0)
+              num = num*j;
+            }
+        }
+        
         if (type=='c' || type=='C'){
             result = (num * 9/5)+32;
             result = result.toFixed(2) + " Farenheit";  /*toFixed --> Redondeo*/
